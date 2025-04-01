e015fa8ff19339aa54719e071c176b615bcbf3af
YOUSSEF <youssef.shoukry@snowflake.com>
2025-04-01 22:38:17 +0000

    commit

---
+++ Working with Git/YOUSSEF_TEST_NOTEBOOK/sample3.java
@@ -0,0 +1,12 @@
+public class MyClass {
+  static void myMethod() {
+    System.out.println("Hello World!");
+  }
+
+  public static void main(String[] args) {
+    myMethod();
+      st
+  }
+}
+
+// Outputs "Hello World!"
